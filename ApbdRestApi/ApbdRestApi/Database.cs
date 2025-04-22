9d29d5e1cd1d8390d4b6d7d19cd774749e60013d
S28548 <s28548@pjwstk.edu.pl>
2025-04-22 23:41:20 +0200

    Added model and controller for visits

--- ApbdRestApi/ApbdRestApi/Database.cs
+++ ApbdRestApi/ApbdRestApi/Database.cs
@@ -12,4 +12,10 @@ public static class Database
         new Animal() { Id = 3, Name = "Hopek", Category = AnimalCategory.Rabbit, FurColor = "Gray", Weight = 5.4f },
         new Animal() { Id = 4, Name = "Cwirek", Category = AnimalCategory.Bird, FurColor = "Brown", Weight = 0.6f },
     };
+    public static List<Visit> Visits = new List<Visit>()
+    {
+        new Visit() { VisitDateTime = DateTime.Now.AddDays(-30), Animal = Animals[0], Description = "Szczepienie", Price = 50 },
+        new Visit() { VisitDateTime = DateTime.Now.AddDays(-30), Animal = Animals[0], Description = "Sterylizacja", Price = 800 },
+        new Visit() { VisitDateTime = DateTime.Now.AddDays(-60), Animal = Animals[1], Description = "Sterylizacja", Price = 600 },
+    };
 }
