ccdfe952fcdd5dc161d7bc4581fa1249ae8b361a
S28548 <s28548@pjwstk.edu.pl>
2025-04-22 23:26:33 +0200

    Added rest api project with basic endpoints and Animal model

---
+++ ApbdRestApi/ApbdRestApi/Models/Animal.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using ApbdRestApi.Enums;
+
+namespace ApbdRestApi.Models;
+
+public class Animal
+{
+    public int Id { get; set; }
+    [Required]
+    public string Name { get; set; }
+    public AnimalCategory Category { get; set; }
+    public float Weight { get; set; }
+    public string FurColor { get; set; }
+}
