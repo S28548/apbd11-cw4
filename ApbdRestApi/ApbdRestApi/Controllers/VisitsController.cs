e5a7980d832087fcd4f47ac72f76d5029cbd1f5e
S28548 <s28548@pjwstk.edu.pl>
2025-04-22 23:41:20 +0200

    Added model and controller for visits

---
+++ ApbdRestApi/ApbdRestApi/Controllers/VisitsController.cs
@@ -0,0 +1,29 @@
+﻿using ApbdRestApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApbdRestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Visits : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetByAnimal([FromBody] Animal animal)
+        {
+            var visits = Database.Visits.Where(v => v.Animal == animal);
+            return Ok(visits);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] Visit visit)
+        {
+            if (!Database.Animals.Contains(visit.Animal))
+            {
+                return NotFound("Animal has not been found.");
+            }
+
+            Database.Visits.Add(visit);
+            return Created();
+        }
+    }
+}
---
+++ ApbdRestApi/ApbdRestApi/Models/Visit.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApbdRestApi.Models;
+
+public class Visit
+{
+    public DateTime VisitDateTime { get; set; }
+    [Required]
+    public Animal Animal { get; set; }
+    public string Description { get; set; }
+    public float Price { get; set; }
+}
